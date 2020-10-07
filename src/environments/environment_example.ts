12c31da194795cbe7d32ae59f654adc42d450be9
Srijan Shrestha <srijaniw@gmail.com>
2020-10-07 21:08:51 +0545

    Setup environement file

--- src/environments/environment_example.ts
+++ src/environments/environment_example.ts
@@ -1,9 +1,4 @@
 export const environment = {
   production: false,
   API_URL: '',
-  HOST: 'blaast.com',
-  API_HOST: 'blaast.com:8000',
-  PROTOCOL: 'http:',
-  VERSION: '1.0',
-  PAYPAL_CLIENT_ID: '******',
 };
