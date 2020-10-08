91063919fe0f9894fe48fd359ca0b2a6004da800
Srijan Shrestha <srijaniw@gmail.com>
2020-10-08 20:38:01 +0545

    Setup diferent guards

--- src/app/shared/directive/background-color.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BackgroundColorDirective } from './background-color.directive';
-
-describe('BackgroundColorDirective', () => {
-  it('should create an instance', () => {
-    const directive = new BackgroundColorDirective();
-    expect(directive).toBeTruthy();
-  });
-});
