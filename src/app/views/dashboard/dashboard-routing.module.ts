3e0a682c0ddc9f9a8eaa7467c523cc70a177f459
Srijan Shrestha <srijaniw@gmail.com>
2020-10-14 15:43:54 +0545

    Change in dashboard module

--- src/app/views/dashboard/dashboard-routing.module.ts
+++ src/app/views/dashboard/dashboard-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { PersonalOverviewComponent } from './components/personal-overview/personal-overview.component';
 import { DashboardContainerComponent } from './dashboard-container/dashboard-container.component';
 
 
@@ -10,57 +9,6 @@ const routes: Routes = [
     path: '',
     component: DashboardContainerComponent,
     children : [
-      {
-        path: '',
-        component: PersonalOverviewComponent
-      },
-      {
-        path: 'personal',
-        component: PersonalOverviewComponent
-      },
-      {
-        path: 'team-dashboard',
-        component: PersonalOverviewComponent
-      },
-      {
-        path: 'department-dashboard',
-        component: PersonalOverviewComponent
-      },
-      {
-        path: 'department-goal',
-        component: PersonalOverviewComponent
-      },
-      {
-        path: 'team-goal',
-        component: PersonalOverviewComponent
-      },
-      {
-        path: 'reports',
-        component: PersonalOverviewComponent
-      },
-      {
-        path: 'department-plan',
-        component: PersonalOverviewComponent
-      },
-      {
-        path: 'team-plan',
-        component: PersonalOverviewComponent
-      },
-      {
-        path: 'add-team-member',
-        component: PersonalOverviewComponent
-      },
-      
-      // {
-      //   path: 'company',
-      //   component: PersonalOverviewComponent,
-      //   canActivate: []
-      // },
-      // {
-      //   path: 'weekly-checkin',
-      //   component: PersonalOverviewComponent,
-      //   canActivate: []
-      // },
     ]
 }];
 
--- src/app/views/dashboard/dashboard.module.ts
+++ src/app/views/dashboard/dashboard.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { PersonalOverviewComponent } from './components/personal-overview/personal-overview.component';
 import { DashboardContainerComponent } from './dashboard-container/dashboard-container.component';
 import { SharedModule } from '../../shared/shared.module';
-import { SlideOutComponent } from './components/slide-out/slide-out.component';
 
 
 @NgModule({
-  declarations: [DashboardComponent, PersonalOverviewComponent, DashboardContainerComponent, SlideOutComponent],
+  declarations: [DashboardComponent, DashboardContainerComponent],
   imports: [
     CommonModule,
     DashboardRoutingModule,
