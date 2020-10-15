19ddc0f6d0e345c2c75f6d10db86c0ecbc0be164
Srijan Shrestha <srijaniw@gmail.com>
2020-10-15 17:05:46 +0545

    Setup email validator

--- src/app/shared/validators/phone.validator.ts
+++ src/app/shared/validators/phone.validator.ts
@@ -1,6 +1,5 @@
 import {FormControl} from '@angular/forms';
 
-
 export function validatePhonePattern(control: FormControl) {
   const localPhonePattern = /^[\0-9]{3}[\s\-]?[0-9]{4}$/;
   const domesticPhonePattern = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
